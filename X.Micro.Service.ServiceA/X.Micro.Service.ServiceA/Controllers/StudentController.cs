b1c2bb08cfc733d3a0c3bafc8c696c671b882024
X.MAX <minaxu01@126.com>
2020-08-19 19:03:53 +0800

    LoadBalancerOptions

--- X.Micro.Service.ServiceA/X.Micro.Service.ServiceA/Controllers/StudentController.cs
+++ X.Micro.Service.ServiceA/X.Micro.Service.ServiceA/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,11 +13,14 @@ namespace X.Micro.Service.ServiceA.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        private IConfiguration _configuration;
+        public StudentController(IConfiguration configuration) => _configuration = configuration;
+
         // GET: api/<StudentController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", _configuration["port"] };
         }
 
         // GET api/<StudentController>/5
